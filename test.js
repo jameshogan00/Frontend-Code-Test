51c252e114670f549bee83deac92b2f4beb23cef
James Hogan <jameshogan00@gmail.com>
2013-08-09 16:43:21 +0200

    front end code test
    (provided at https://github.com/thrashr888/Frontend-Code-Test)

--- test.js
+++ test.js
@@ -1,1 +1,16 @@
-/* JS goes here */
+$(document).ready(function(){
+
+	$('li.side-link').click(function(){
+		setTimeout(function(){
+		     $('.main-content-sidebar').toggleClass('toggle-visibility');
+		     }, 3000);
+	});
+   
+    $('li.blue-link').mouseenter(function(){
+         $(this).siblings().css('color', 'blue');
+    });
+
+    $('li.blue-link').mouseleave(function(){
+         $(this).siblings().css('color', 'white');
+    });
+});
